85d8b5cb9c3fc5a6fc97b2d6130bb1a1e2b04e3a
Тарасенко Никита Сергеевич <NSTarasenko@sberbank.ru>
2021-12-10 03:47:46 +0300

    Refactoring code

--- src/main/kotlin/com/webshop/server/service/ShopService.kt
+++ src/main/kotlin/com/webshop/server/service/ShopService.kt
@@ -4,7 +4,6 @@ import com.webshop.server.model.GoodsModel
 import com.webshop.server.repository.ShopRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.lang.Exception
 
 @Service
 class ShopService {
